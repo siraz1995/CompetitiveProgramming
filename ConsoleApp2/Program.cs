2f94df65c3f74a387fb003ef71af6ee028e3c516
Sirazul Islam <Sirazul Islam@DESKTOP-0BC93V6>
2023-08-12 00:05:10 +0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static int p1 = 0, p2 = 1, p3;
+        static void Main(string[] args)
+        {
+            //[generate fibonacci number]
+            //Console.WriteLine("please provide input value");
+            //int count = int.Parse(Console.ReadLine());
+            //Console.Write(p1 + " " + p2);
+            //for (int i = 2; i < count; i++)
+            //{
+            //    p3 = p1 + p2;
+            //    Console.Write(" " + p3);
+            //    p1 = p2;
+            //    p2 = p3;
+            //}
+
+            Console.WriteLine();
+        }
+    }
+}
