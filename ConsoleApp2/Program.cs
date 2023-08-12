5d1caa924d85dc6d4ac5b6485213de9aba234105
Sirazul Islam <sirazulislam.bd.2014@gmail.com>
2023-08-12 17:59:58 +0600

    yjhg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,13 +2,15 @@
 {
     internal class Program
     {
-        static int p1 = 0, p2 = 1, p3;
+        //static int p1 = 0, p2 = 1, p3;
         static void Main(string[] args)
         {
             //[generate fibonacci number]
             //Console.WriteLine("please provide input value");
-            //int count = int.Parse(Console.ReadLine());
+            //int count = Int32.Parse(Console.ReadLine());
             //Console.Write(p1 + " " + p2);
+            /////or method use
+            //Fibonacci(count - 2);
             //for (int i = 2; i < count; i++)
             //{
             //    p3 = p1 + p2;
@@ -17,7 +19,47 @@
             //    p2 = p3;
             //}
 
-            Console.WriteLine();
+
+            /////Factorial Number
+            //Console.WriteLine("Please provide factorial number.");
+            //int factorialNum =Int32.Parse(Console.ReadLine());
+            //int factResult = 1;
+            //for (int i = 1; i <= factorialNum; i++)
+            //{
+            //    factResult = factResult * i;
+            //}
+            ////Or
+            //for (int i =factorialNum; i>=factorialNum; i--)
+            //{
+            //    factResult = factResult * i;
+            //}
+            //Console.WriteLine("{0}! is {1}",factorialNum,factResult);
+
+            ////Sum of digits of a given number
+            //Console.WriteLine("Please provide input number");
+            //int inputNumber= Int32.Parse(Console.ReadLine());
+            //int backupValue = inputNumber;
+            //int sum = 0, remainder = 0;
+            //while (inputNumber != 0)
+            //{
+            //    remainder = inputNumber % 10;
+            //    sum += remainder;
+            //    inputNumber=inputNumber / 10;
+            //}
+            //Console.WriteLine("sum of {0} is {1}",backupValue,sum);
+
         }
+
+        //private static void Fibonacci(int count)
+        //{
+        //    if (count > 0)
+        //    {
+        //        p3 = p1 + p2;
+        //        Console.Write(" " + p3);
+        //        p1 = p2;
+        //        p2 = p3;
+        //        Fibonacci(count - 1);
+        //    }
+        //}
     }
 }
