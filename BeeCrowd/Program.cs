bb4f9810b531d3ff89cad98869391c645d63f711
siraz1995 <sirazulislam.bd.2014@gmail.com>
2023-12-02 19:31:42 +0600

    BeeCrowd

---
+++ BeeCrowd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BeeCrowd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prolem no 1000
+            Console.WriteLine("Hello World!");
+            //problem no 1001
+            int A, B, X;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            X = A + B;
+            Console.WriteLine("X =" + X);
+            //problem no 1003
+
+        }
+    }
+}
